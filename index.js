7e7dcc42875826598f79ed153e993d0e073bd187
icika-max <icika-max>
2022-11-25 11:31:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = (drivers) =>
+{
+    const newArray = drivers.slice(0,2)
+    return (newArray)
+}
+const returnLastTwoDrivers = (drivers) =>{
+    const otherArray = drivers.slice(-2)
+    return (otherArray)
+}
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+const createFareMultiplier= fare =>
+{return () => fare*5
+
+}
+const  fareDoubler= double=>
+{
+    return double*2
+}
+const fareTripler= tripler=>
+{
+    return  tripler*3
+
+}
+const selectDifferentDrivers=(drivers, returnFirstTwoDrivers)=>
+{
+    return returnFirstTwoDrivers(drivers)
+}
+
